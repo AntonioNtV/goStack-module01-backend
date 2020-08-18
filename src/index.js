cf6c60b820252283183315925ab2f21103624c88
AntonioNtV <antoniontv18@gmail.com>
2020-08-17 22:54:21 -0300

    Funcionalidade de deletar um projeto

--- src/index.js
+++ src/index.js
@@ -67,12 +67,17 @@ app.put('/projects/:id', (request, response) => {
 app.delete('/projects/:id', (request, response) => {
     const { id } = request.params
 
-    console.log('id:', id);
+    const projectIndex = projects.findIndex( project => project.id === id)
 
-    return response.json([
-        'Projeto 4',
-        'Projeto 3'
-    ])
+    if (projectIndex < 0) {
+        return response.status(400).json({
+            error: 'Project not found'
+        })
+    }
+
+    projects.splice(projectIndex, 1)
+
+    return response.status(204).send()
 })
 
 app.use(express.json())
