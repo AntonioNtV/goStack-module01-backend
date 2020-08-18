1432e3d8f5d6573a137532042553f45df4b10c15
AntonioNtV <antoniontv18@gmail.com>
2020-08-17 22:52:29 -0300

    Funcionalidade de editar um projeto

--- src/index.js
+++ src/index.js
@@ -19,17 +19,12 @@ app.use(express.json())
   * Route Params: Identificar recursos (Atualizar ou Deletar).
   * Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
   */
+
 const projects = []
 
 app.get('/projects', (request, response) => {
     const { title, owner } = request.query
-
-    console.log(title, owner);
-
-    return response.json([
-        'Projeto 1',
-        'Projeto 2'
-    ])
+    return response.json(projects)
 })
 
 app.post('/projects', (request, response) => {
@@ -48,14 +43,25 @@ app.post('/projects', (request, response) => {
 
 app.put('/projects/:id', (request, response) => {
     const { id } = request.params
+    const { title, owner } = request.body
+    
+    const projectIndex = projects.findIndex( project => project.id === id)
 
-    console.log('id:', id);
+    if (projectIndex < 0) {
+        return response.status(400).json({
+            error: 'Project not found'
+        })
+    }
 
-    return response.json([
-        'Projeto 4',
-        'Projeto 2',
-        'Projeto 3'
-    ])
+    const project = {
+        id,
+        title,
+        owner
+    }
+
+    projects[projectIndex] = project
+
+    return response.json(project)
 })
 
 app.delete('/projects/:id', (request, response) => {
